d4ca0ac9fa2377726ec9509cf8eb4fede285182a
jbrmmg <jason@jbrmmg.me.uk>
2026-08-15 12:31:54 +0100

    JBR-716 recipe management.

--- src/main/java/com/jbr/middletier/recipe/config/SpaController.java
+++ src/main/java/com/jbr/middletier/recipe/config/SpaController.java
@@ -6,8 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SpaController {
 
-    // Forward all non-API requests to index.html so Angular routing works
-    @RequestMapping(value = {"/{path:^(?!api|actuator|h2|swagger-ui|v3).*}", "/{path:^(?!api|actuator|h2|swagger-ui|v3).*}/**"})
+    // Forward Angular routes to index.html. Excludes API paths and any path containing
+    // a dot (file extensions) so static resources are served by the default handler.
+    @RequestMapping(value = {
+        "/{path:^(?!api|actuator|h2|swagger-ui|v3)(?!.*\\.)[^\\s]*}",
+        "/{path:^(?!api|actuator|h2|swagger-ui|v3)(?!.*\\.)[^\\s]*}/**"
+    })
     public String forward() {
         return "forward:/index.html";
     }
